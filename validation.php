a635aec9a664ca9fa64fcd67581d103d02332489
Nicolas Roblin <nicolasroblin@MacBook-Pro-de-Nicolas.local>
2016-09-13 19:08:37 +0200

    PHP : Email confirmation works for registration

--- validation.php
+++ validation.php
@@ -3,11 +3,12 @@ require('config/config.php');
 require('model/functions.fn.php');
 session_start();
 
-if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['password2'])  &&
-	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && isset($_POST['password2'])) {
+if(	isset($_POST['username']) && isset($_POST['email'])&& isset($_POST['email2']) && isset($_POST['password']) && isset($_POST['password2'])  &&
+	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['email2']) && !empty($_POST['password']) && !empty($_POST['password2'])) {
 
 	$username = htmlspecialchars($_POST["username"]);
 	$email = htmlspecialchars($_POST["email"]);
+    $email2 = htmlspecialchars($_POST["email2"]);
 	$password = htmlspecialchars($_POST["password"]);
     $password2 = htmlspecialchars($_POST["password2"]);
 
@@ -19,9 +20,16 @@ if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['passwor
 	if(sizeof($members) == 0){
 
 	    if ($password == $password2){
-            $req=$db->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
-            $req->execute(array("username"=>$username, "email"=>$email, "password"=>$password));
-            header('Location: login.php');
+            if ($email == $email2){
+
+	            $req=$db->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
+                $req->execute(array("username"=>$username, "email"=>$email, "password"=>$password));
+                header('Location: login.php');
+	        }else{
+                $_SESSION['message'] = 'Erreur : Vos emails ne sont pas identiques !';
+                header('Location: register.php');
+            }
+            
 	    }else{
             $_SESSION['message'] = 'Erreur : Vos mots de passe ne sont pas identiques !';
             header('Location: register.php');
