80d2c519e2727301d3e8a9ac402c871f254199c0
Nicolas Roblin <nicolasroblin@MacBook-Pro-de-Nicolas.local>
2016-09-13 18:01:47 +0200

    PHP : Registration works (you can't create two accounts with the same username or email)

--- validation.php
+++ validation.php
@@ -6,7 +6,24 @@ session_start();
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
+	$username = htmlspecialchars($_POST["username"]);
+	$email = htmlspecialchars($_POST["email"]);
+	$password = htmlspecialchars($_POST["password"]);
+
+	$req=$db->prepare("SELECT id FROM users WHERE username LIKE :username OR email LIKE :email");
+	$req->execute(array("username"=>$username, "email"=>$email));
+
+	$members=$req->fetchAll();
+
+	if(sizeof($members) == 0){
+		$req=$db->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
+		$req->execute(array("username"=>$username, "email"=>$email, "password"=>$password));
+        header('Location: login.php');
+	}else{
+		$_SESSION['message'] = 'Erreur : Votre username ou votre email existe déjà !';
+        header('Location: register.php');
+	}
+
 
 }else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
