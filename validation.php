290c987df992b08de3bb11c5731152e9cf71dcd2
Nicolas Roblin <nicolasroblin@MacBook-Pro-de-Nicolas.local>
2016-09-13 18:18:12 +0200

    PHP : Password confirmation works for registration

--- validation.php
+++ validation.php
@@ -3,12 +3,13 @@ require('config/config.php');
 require('model/functions.fn.php');
 session_start();
 
-if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
-	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['password2'])  &&
+	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && isset($_POST['password2'])) {
 
 	$username = htmlspecialchars($_POST["username"]);
 	$email = htmlspecialchars($_POST["email"]);
 	$password = htmlspecialchars($_POST["password"]);
+    $password2 = htmlspecialchars($_POST["password2"]);
 
 	$req=$db->prepare("SELECT id FROM users WHERE username LIKE :username OR email LIKE :email");
 	$req->execute(array("username"=>$username, "email"=>$email));
@@ -16,16 +17,22 @@ if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['passwor
 	$members=$req->fetchAll();
 
 	if(sizeof($members) == 0){
-		$req=$db->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
-		$req->execute(array("username"=>$username, "email"=>$email, "password"=>$password));
-        header('Location: login.php');
-	}else{
-		$_SESSION['message'] = 'Erreur : Votre username ou votre email existe déjà !';
+
+	    if ($password == $password2){
+            $req=$db->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
+            $req->execute(array("username"=>$username, "email"=>$email, "password"=>$password));
+            header('Location: login.php');
+	    }else{
+            $_SESSION['message'] = 'Erreur : Vos mots de passe ne sont pas identiques !';
+            header('Location: register.php');
+        }
+        
+    }else{
+        $_SESSION['message'] = 'Erreur : Votre username ou votre email existe déjà !';
         header('Location: register.php');
-	}
+    }
 
-
-}else{ 
+}else{
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
 	header('Location: register.php');
 }
