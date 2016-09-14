8260620de333760096bc4b9bad1e5b704694e929
Julien Salliot <julien.salliot@gmail.com>
2016-09-14 16:57:17 +0200

    HTML/PHP : main contact page created / profile block copied into contact
    Just to start something

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+require('config/config.php');
+require('model/functions.fn.php');
+
+
+
+include 'view/_header.php';
+include 'view/edit_image.php';
+include 'view/_footer.php';
