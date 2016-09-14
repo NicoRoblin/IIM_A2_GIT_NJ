e0671d7949641b1314491aad33de1b3f55a69725
Nicolas Roblin <nicolas.roblin@devinci.fr>
2016-09-14 17:02:59 +0200

    HTML : Target blank for Facebook link

--- view/_topbar.php
+++ view/_topbar.php
@@ -11,7 +11,7 @@
 						<a href="edit_image.php" class="btn">Mon profil</a>
 						<a href="contact.php" class="btn">Contact</a>
 						<a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Ajouter une musique</a>
-						<a href="https://www.facebook.com/groups/293834574317011/?fref=ts" class="btn"><i class="fa fa-facebook"></i></a>
+						<a href="https://www.facebook.com/groups/293834574317011/?fref=ts" target="_blank" class="btn"><i class="fa fa-facebook"></i></a>
 						<a href="https://twitter.com/soundcloud?lang=fr" target="_blank" class="btn"><i class="fa fa-twitter"></i></a>
 						<h4 class="welcome">Bienvenue <?php echo $_SESSION["username"] ?> <span>!</span></h4>
 					</div>
