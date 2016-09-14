88d2913f16d9265c3362c69effdec89191ca881d
Julien Salliot <julien.salliot@gmail.com>
2016-09-14 18:02:08 +0200

    HTML : form created

--- view/contact.php
+++ view/contact.php
@@ -5,15 +5,16 @@
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div id="musicfeed">
                 <h1><i class="fa fa-pencil"></i> Contactez nous !</h1>
-                <div class="block animated fadeInDown">
-                    <div class="row">
-                        <div class="col-xs-2 col-sm-2 col-md-1 col-lg-1">
 
-                        </div>
-                        <div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
+                            <form id="form" class="topBefore">
 
-                        </div>
-                    </div>
+                                <input class="input" id="name" type="text" placeholder="USERNAME">
+                                <input class="input" id="email" type="text" placeholder="E-MAIL">
+                                <textarea id="message" type="text" placeholder="MESSAGE"></textarea>
+                                <input class="input" id="submit" type="submit" value="GO!">
+
+                            </form>
+
                 </div>
             </div>
         </div>
