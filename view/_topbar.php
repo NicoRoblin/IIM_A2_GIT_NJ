4dd24d380062e643492b65c319edd811b12620fd
Nicolas Roblin <nicolas.roblin@devinci.fr>
2016-09-14 16:06:30 +0200

    HTML : Facebook link added on the dashboard's topbar

--- view/_topbar.php
+++ view/_topbar.php
@@ -10,6 +10,7 @@
 						<a href="logout.php" class="btn">Déconnexion</a>
 						<a href="edit_image.php" class="btn">Mon profil</a>
 						<a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Ajouter une musique</a>
+						<a href="" class="btn"><i class="fa fa-facebook"></i></a>
 						<h4 class="welcome">Bienvenue <?php echo $_SESSION["username"] ?> <span>!</span></h4>
 					</div>
 				</div>
