f42057515bb8a768cf5845cacf13ef996f6750c2
Julien Salliot <julien.salliot@gmail.com>
2016-09-15 21:08:06 +0200

    PHP : Test for validation (send email)

--- send_form_email.php
+++ send_form_email.php
@@ -18,22 +18,6 @@ if(isset($_POST['email'])) {
 
 
 
-    function died($error) {
-
-        // your error code can go here
-
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-
-        echo "These errors appear below.<br /><br />";
-
-        echo $error."<br /><br />";
-
-        echo "Please go back and fix these errors.<br /><br />";
-
-        die();
-
-    }
-
 
 
     // validation expected data exists
@@ -43,8 +27,6 @@ if(isset($_POST['email'])) {
         !isset($_POST['email']) ||
 
         !isset($_POST['comments'])) {
-
-        died('We are sorry, but there appears to be a problem with the form you submitted.');
 
     }
 
@@ -80,12 +62,6 @@ if(isset($_POST['email'])) {
     if(strlen($comments) < 2) {
 
         $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-
-    }
-
-    if(strlen($error_message) > 0) {
-
-        died($error_message);
 
     }
 
